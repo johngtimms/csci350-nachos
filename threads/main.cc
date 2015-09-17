94e64647908344fb635b38759dac58f3373a0807
Yetsebaot Sisay <sisay@usc.edu>
2015-09-17 16:17:40 -0700

    began implementing customer class

--- threads/main.cc
+++ threads/main.cc
@@ -1,4 +1,4 @@
-// main.cc 
+// main.cc
 //	Bootstrap code to initialize the operating system kernel.
 //
 //	Allows direct calls into internal operating system functions,
@@ -31,7 +31,7 @@
 //    -p prints a Nachos file to stdout
 //    -r removes a Nachos file from the file system
 //    -l lists the contents of the Nachos directory
-//    -D prints the contents of the entire file system 
+//    -D prints the contents of the entire file system
 //    -t tests the performance of the Nachos file system
 //
 //  NETWORK
@@ -43,7 +43,7 @@
 //  Some of the flags are interpreted here; some in system.cc.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #define MAIN
@@ -66,14 +66,14 @@ extern void Part2(void), TestSuite(void);
 
 //----------------------------------------------------------------------
 // main
-// 	Bootstrap the operating system kernel.  
-//	
+// 	Bootstrap the operating system kernel.
+//
 //	Check command line arguments
 //	Initialize data structures
 //	(optionally) Call test procedure
 //
 //	"argc" is the number of command line arguments (including the name
-//		of the command) -- ex: "nachos -d +" -> argc = 3 
+//		of the command) -- ex: "nachos -d +" -> argc = 3
 //	"argv" is an array of strings, one for each command line argument
 //		ex: "nachos -d +" -> argv = {"nachos", "-d", "+"}
 //----------------------------------------------------------------------
@@ -81,7 +81,7 @@ extern void Part2(void), TestSuite(void);
 int
 main(int argc, char **argv)
 {
-    int argCount;			// the number of arguments 
+    int argCount;			// the number of arguments
 					// for a particular command
 
     DEBUG('t', "Entering main");
@@ -91,15 +91,15 @@ main(int argc, char **argv)
 	argCount = 1;
         if (!strcmp(*argv, "-z"))               // print copyright
             printf (copyright);
-        
+
 #ifdef THREADS
         //ThreadTest();
         if (!strcmp(*argv, "-T"))               // Test Suite
             TestSuite();
-        //    if (!strcmp(*argv, "-P2"))               // Problem 2
-        //        Problem2();
+        if (!strcmp(*argv, "-P2"))               // Problem 2
+            Part2();
 #endif
-        
+
 #ifdef USER_PROGRAM
         if (!strcmp(*argv, "-x")) {        	// run a user program
 	    ASSERT(argc > 1);
@@ -113,8 +113,8 @@ main(int argc, char **argv)
 	        ConsoleTest(*(argv + 1), *(argv + 2));
 	        argCount = 3;
 	    }
-	    interrupt->Halt();		// once we start the console, then 
-					// Nachos will loop forever waiting 
+	    interrupt->Halt();		// once we start the console, then
+					// Nachos will loop forever waiting
 					// for console input
 	}
 #endif // USER_PROGRAM
@@ -143,7 +143,7 @@ main(int argc, char **argv)
         if (!strcmp(*argv, "-o")) {
 	    ASSERT(argc > 1);
             Delay(2); 				// delay for 2 seconds
-						// to give the user time to 
+						// to give the user time to
 						// start up another nachos
             MailTest(atoi(*(argv + 1)));
             argCount = 2;
@@ -151,7 +151,7 @@ main(int argc, char **argv)
 #endif // NETWORK
     }
 
-    currentThread->Finish();	// NOTE: if the procedure "main" 
+    currentThread->Finish();	// NOTE: if the procedure "main"
 				// returns, then the program "nachos"
 				// will exit (as any other normal program
 				// would).  But there may be other
--- threads/threadtest.cc
+++ threads/threadtest.cc
@@ -1,20 +1,22 @@
-// threadtest.cc 
+// threadtest.cc
 //	Simple test case for the threads assignment.
 //
 //	Create two threads, and have them context switch
-//	back and forth between themselves by calling Thread::Yield, 
+//	back and forth between themselves by calling Thread::Yield,
 //	to illustratethe inner workings of the thread system.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
 #include "system.h"
+#include <string>
+#include <iostream>
 
 //----------------------------------------------------------------------
 // SimpleThread
-// 	Loop 5 times, yielding the CPU to another ready thread 
+// 	Loop 5 times, yielding the CPU to another ready thread
 //	each iteration.
 //
 //	"which" is simply a number identifying the thread, for debugging
@@ -25,7 +27,7 @@ void
 SimpleThread(int which)
 {
     int num;
-    
+
     for (num = 0; num < 5; num++) {
 	printf("*** thread %d looped %d times\n", which, num);
         currentThread->Yield();
@@ -34,7 +36,7 @@ SimpleThread(int which)
 
 //----------------------------------------------------------------------
 // ThreadTest
-// 	Set up a ping-pong between two threads, by forking a thread 
+// 	Set up a ping-pong between two threads, by forking a thread
 //	to call SimpleThread, and then calling SimpleThread ourselves.
 //----------------------------------------------------------------------
 
@@ -84,7 +86,7 @@ Lock t1_l1("t1_l1");		  // the lock tested in Test 1
 void t1_t1() {
     t1_l1.Acquire();
     t1_s1.V();  // Allow t1_t2 to try to Acquire Lock
-    
+
     printf ("%s: Acquired Lock %s, waiting for t3\n",currentThread->getName(),
             t1_l1.getName());
     t1_s3.P();
@@ -101,14 +103,14 @@ void t1_t1() {
 //     This thread will wait on the held lock.
 // --------------------------------------------------
 void t1_t2() {
-    
+
     t1_s1.P();	// Wait until t1 has the lock
     t1_s2.V();  // Let t3 try to acquire the lock
-    
+
     printf("%s: trying to acquire lock %s\n",currentThread->getName(),
            t1_l1.getName());
     t1_l1.Acquire();
-    
+
     printf ("%s: Acquired Lock %s, working in CS\n",currentThread->getName(),
             t1_l1.getName());
     for (int i = 0; i < 10; i++)
@@ -124,9 +126,9 @@ void t1_t2() {
 //     This thread will try to release the lock illegally
 // --------------------------------------------------
 void t1_t3() {
-    
+
     t1_s2.P();	// Wait until t2 is ready to try to acquire the lock
-    
+
     t1_s3.V();	// Let t1 do it's stuff
     for ( int i = 0; i < 3; i++ ) {
         printf("%s: Trying to release Lock %s\n",currentThread->getName(),
@@ -206,9 +208,9 @@ void t3_waiter() {
 //     one t3_signaller will be released
 // --------------------------------------------------
 void t3_signaller() {
-    
+
     // Don't signal until someone's waiting
-    
+
     for ( int i = 0; i < 5 ; i++ )
         t3_s1.P();
     t3_l1.Acquire();
@@ -252,9 +254,9 @@ void t4_waiter() {
 //     All t4_waiters will be released
 // --------------------------------------------------
 void t4_signaller() {
-    
+
     // Don't broadcast until someone's waiting
-    
+
     for ( int i = 0; i < 5 ; i++ )
         t4_s1.P();
     t4_l1.Acquire();
@@ -333,42 +335,42 @@ void TestSuite() {
     Thread *t;
     char *name;
     int i;
-    
+
     // Test 1
-    
+
     printf("Starting Test 1\n");
-    
+
     t = new Thread("t1_t1");
     t->Fork((VoidFunctionPtr)t1_t1,0);
-    
+
     t = new Thread("t1_t2");
     t->Fork((VoidFunctionPtr)t1_t2,0);
-    
+
     t = new Thread("t1_t3");
     t->Fork((VoidFunctionPtr)t1_t3,0);
-    
+
     // Wait for Test 1 to complete
     for (  i = 0; i < 2; i++ )
         t1_done.P();
-    
+
     // Test 2
-    
+
     printf("Starting Test 2.  Note that it is an error if thread t2_t2\n");
     printf("completes\n");
-    
+
     t = new Thread("t2_t1");
     t->Fork((VoidFunctionPtr)t2_t1,0);
-    
+
     t = new Thread("t2_t2");
     t->Fork((VoidFunctionPtr)t2_t2,0);
-    
+
     // Wait for Test 2 to complete
     t2_done.P();
-    
+
     // Test 3
-    
+
     printf("Starting Test 3\n");
-    
+
     for (  i = 0 ; i < 5 ; i++ ) {
         name = new char [20];
         sprintf(name,"t3_waiter%d",i);
@@ -377,15 +379,15 @@ void TestSuite() {
     }
     t = new Thread("t3_signaller");
     t->Fork((VoidFunctionPtr)t3_signaller,0);
-    
+
     // Wait for Test 3 to complete
     for (  i = 0; i < 2; i++ )
         t3_done.P();
-    
+
     // Test 4
-    
+
     printf("Starting Test 4\n");
-    
+
     for (  i = 0 ; i < 5 ; i++ ) {
         name = new char [20];
         sprintf(name,"t4_waiter%d",i);
@@ -394,22 +396,175 @@ void TestSuite() {
     }
     t = new Thread("t4_signaller");
     t->Fork((VoidFunctionPtr)t4_signaller,0);
-    
+
     // Wait for Test 4 to complete
     for (  i = 0; i < 6; i++ )
         t4_done.P();
-    
+
     // Test 5
-    
+
     printf("Starting Test 5.  Note that it is an error if thread t5_t1\n");
     printf("completes\n");
-    
+
     t = new Thread("t5_t1");
     t->Fork((VoidFunctionPtr)t5_t1,0);
-    
+
     t = new Thread("t5_t2");
     t->Fork((VoidFunctionPtr)t5_t2,0);
-    
+
+}
+Lock senatorOutsideLineLock("senator outside line lock");
+int senatorsOutside = 0;
+Lock senatorInsideLock("senator inside line lock");
+bool senatorInside = false;
+Lock customersOutsideLineLock("customer outside line lock");
+Lock appBribeLineLock("app bribe line lock");
+Lock appPlebLineLock("app pleb ine lock");
+Lock picBribeLineLock("pic bribe line lock");
+Lock picPlebLineLock("pic pleb line lock");
+int appBribeLineLength = 0;
+int appPlebLineLength = 0;
+int picBribeLineLength = 0;
+int picPlebLineLength = 0;
+int customersOutside = 0;
+using namespace std;
+
+class Customer {
+public:
+    char* ssn;
+    string name;
+    int cash;
+    Lock *access;
+    Condition *customer_cv;
+    bool isSenator;
+
+    bool hasApp;
+    bool hasPic;
+    bool hasPassport;
+    bool seenAppClerk;
+    bool seenPicClerk;
+    bool seenPassportClerk;
+    bool willBribePic;
+    bool willBribeApp;
+
+    Customer(string givenName)
+    {
+        name = givenName;
+        cout<<"Customer created with name: "<<name<<endl;
+        access = new Lock("customer access lock");
+        isSenator = false;
+        customer_cv= new Condition("t2_c1");
+    }
+
+    void main() {
+        cout<<"inside class"<<endl;
+        access->Acquire();
+
+        //Determine money and bribes
+
+        //do senator stuff
+        //choose if senator here
+
+        //Ready to enter, choose a line
+        int appLineLength;
+        int picLineLength;
+
+        if (willBribeApp) {
+                appBribeLineLock.Acquire();
+                appLineLength = appBribeLineLength;
+        } else {
+                appPlebLineLock.Acquire();
+                appLineLength = appPlebLineLength;
+        }
+        if (willBribePic) {
+                picBribeLineLock.Acquire();
+                picLineLength = picBribeLineLength;
+        } else {
+                picPlebLineLock.Acquire();
+                picLineLength = picPlebLineLength;
+        }
+
+        appBribeLineLock.Release();
+        appPlebLineLock.Release();
+        picBribeLineLock.Release();
+        picPlebLineLock.Release();
+
+        access->Release();
+
+        // Enter the first line chosen
+        if (appLineLength <= picLineLength) {
+          doApp();
+          enterPicLine();
+        } else {
+          enterPicLine();
+          doApp();
+        }
+
+
+    }
+
+    void doApp() {
+        bool senatord = false;
+        senatord = enterAppLine();
+        while (senatord) {
+            enterAppLine();
+        }
+        interactWithAppClerk();
+    }
+
+    bool enterAppLine() {
+        int senatord = false;
+            
+        if (willBribeApp) {
+            appBribeLineLock.Acquire();
+            appBribeLineLength += 1;
+            customer_cv->Wait(&appBribeLineLock);
+            appBribeLineLength -= 1;
+            appBribeLineLock.Release();
+        } else {
+            appPlebLineLock.Acquire();
+            appPlebLineLength += 1;
+            customer_cv->Wait(&appPlebLineLock);
+            appPlebLineLength -= 1;
+            appPlebLineLock.Release();
+        }
+        
+        senatorInsideLock.Acquire();
+        if (senatorInside) {
+            customersOutsideLineLock.Acquire();
+            customersOutside += 1;
+            customer_cv->Wait(&customersOutsideLineLock);
+            customersOutside -= 1;
+            customersOutsideLineLock.Release();
+            senatord = true;
+        }
+        senatorInsideLock.Release();
+        
+        return senatord;
+    }
+
+    void enterPicLine() {
+
+    }
+
+    void interactWithAppClerk() {
+        cout<<"About to begin interacting with app clerk"<<endl;
+    }
+
+
+
+};
+
+void customerMain() {
+    Customer customer1("customer 1");
+    customer1.main();
+    //customer.access
+}
+
+void Part2() {
+    Thread *customerThread;
+    customerThread = new Thread("customer");
+    customerThread->Fork((VoidFunctionPtr)customerMain,0);
+
 }
 #endif
-
