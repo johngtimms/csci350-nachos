bb4fb47f0a03743dfa53b3daeee6237893467d40
Yetsebaot Sisay <sisay@usc.edu>
2015-10-21 01:01:30 -0700

    still working on converting passport officce

--- test/passportOffice.c
+++ test/passportOffice.c
@@ -8,6 +8,27 @@ struct Customer;
 struct Clerk;
 struct Manager;
 
+int senatorOutsideLineLock;
+int senatorOutsideLineCV;
+int senatorsOutside = 0;
+int senatorInsideLock;
+bool senatorInside = false;
+int customerOutsideLineLock;
+int customerOutsideLineCV;
+int customersOutside = 0;
+int numCustomers, numApplicationClerks, numPictureClerks, numPassportClerks, numCashiers, numSenators;
+
+int customerCounter = 0;
+
+bool runningTest1 = false;
+bool runningTest2 = false;
+bool runningTest3 = false;
+bool runningTest4 = false;
+bool runningTest5 = false;
+bool runningTest6 = false;
+bool runningTest7a = false;
+bool runningTest7b = false;
+
 
 typedef struct Customer {
 	
@@ -26,6 +47,8 @@ typedef struct Customer {
 	bool likedPic;
 
 } Customer;
+
+Customer* customers[10];
 
 Customer* newCustomer(int index, bool _isSenator){
 	struct Customer *customer;
@@ -47,37 +70,76 @@ Customer* newCustomer(int index, bool _isSenator){
 	return customer;
 }
 
-void runCustomer(Customer customer){
+void doApplication(Customer *customer){
 
-	Exit(0);
 }
+void doPicture(Customer *customer){
+
+}
+void doPassport(Customer *customer){
+	/*
+	waitInLine(customer, passportClerks, numPassportClerks);
+	Clerk* clerk = passportClerks[clerkID];
+	cout<<clerk->getName()<<" has signalled "<<this->getName()<<" to come to their counter."<<endl;
+	clerk->clerkLock->Acquire();
+    clerk->customer = this;
+    //cout << getName() << " currently with " << clerk->getName() << endl;
+    clerk->clerkCV->Signal(clerk->clerkLock); 
+    clerk->clerkCV->Wait(clerk->clerkLock); 
+    if(hasApp && hasPic) {
+        //cout << getName() << " accepted passport from " << clerk->getName() << endl;
+        //certifiedByPassportClerk = true; passport clerk should be setting this
+        //cout << getName() << " finished with " << clerk->getName() << endl;
+        clerk->clerkCV->Signal(clerk->clerkLock);
+        clerk->clerkLock->Release();
+    } else {
+        clerk->clerkCV->Signal(clerk->clerkLock);
+        clerk->clerkLock->Release();
+        int wait = rand() % ((100 - 20) + 1) + 20;
+        for(int i = 0; i < wait; i++) 
+            Yield();
+    }
+    */
+
+}
+void doCashier(Customer *customer){
+
+}
+
+void runCustomer(){
+	Customer *customer = newCustomer(customerCounter, false);
+	customers[customerCounter] = customer;
+	Print("running customer: %i\n",customerCounter);
+	customerCounter = customerCounter + 1;
+	/*Exit(0);*/
+}
+
+
 
 int main() {
+	int k;
+	senatorOutsideLineLock = CreateLock();
+	senatorOutsideLineCV = CreateCondition();
+	senatorInsideLock = CreateLock();
+	customerOutsideLineLock = CreateLock();
+	customerOutsideLineCV = CreateCondition();
+	
+	numCustomers = 10;
 
-	Customer *customer = newCustomer(0, false);
 	
-	Fork(&runCustomer);
 	
+	for(k = 0 ; k < numCustomers ; k++) {
+		Fork(&runCustomer);
+	}
 	
 
 }
+
 
 
 /*
-void doApplication(struct Customer customer){
 
-}
-void doPicture(){
-
-}
-void doPassport(){
-
-}
-void doCashier(){
-
-}
-
-void waitInLine(Clerk** clerks, int numClerks){
+void waitInLine(Customer *customer, Clerk** clerks, int numClerks){
 
 }
 void chooseLine(Clerk** clerks, int numClerks){
@@ -86,9 +148,9 @@ void chooseLine(Clerk** clerks, int numClerks){
 bool enterLine(Clerk* clerk){
 
 }
+
+
 */
-
-
 
 
 
