b0cb6bc7c5d1ef7b051adbe5166b7cfa09580ea9
Nicholas Roubal <nroubal@usc.edu>
2015-10-21 23:53:57 -0700

    delete network

--- network/test/tester.c
+++
@@ -1,6 +0,0 @@
-#include "syscall.h"
-
-int main() {
-	Print("Inside tester\n", 14);
-	Exit(0);	
-}
--- network/test/testexec.c
+++
@@ -1,7 +0,0 @@
-#include "syscall.h"
-
-int main() {
-	int a, b, c;
-	a = Exec("../test/tester", 14);
-	b = Exec("../test/tester2", 15);
-}
