8943d20080da16ff94fbdf701a7c3b210a1b885f
Yetsebaot Sisay <sisay@usc.edu>
2015-12-03 20:09:18 -0800

    converted networkTests and applicationClerk to work with new syscalls that include index of customers/clerks or -1 if not needed

--- test/applicationclerk.c
+++ test/applicationclerk.c
@@ -7,83 +7,83 @@ void runApplicationClerk() {
 	int myCustomer;
 	Print("Running ApplicationClerk: %i\n",i);
 	while(true) {
-    	/*if(applicationClerks[i].senatorLineLength > 0) {*/
-    	if(GetMV(applicationClerks[i].senatorLineLength) > 0) {
-    		Acquire(applicationClerks[i].senatorLineLock);
-            Signal(applicationClerks[i].senatorLineCV, applicationClerks[i].senatorLineLock);     /* Signal Customer to exit line */
+    	/*if(applicationClerk.senatorLineLength > 0) {*/
+    	if(GetMV(applicationClerk.senatorLineLength, i) > 0) {
+    		Acquire(applicationClerk.senatorLineLock, i);
+            Signal(applicationClerk.senatorLineCV, i, applicationClerk.senatorLineLock, i);     /* Signal Customer to exit line */
             Print("ApplicationClerk %i has signalled a Senator to come to their counter\n", i);
-            Acquire(applicationClerks[i].clerkLock); 
-            Release(applicationClerks[i].senatorLineLock);
-            Wait(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);      /* Wait for customer to give application */
-            /*myCustomer = applicationClerks[i].customerID;*/
-            myCustomer = GetMV(applicationClerks[i].customerID);
+            Acquire(applicationClerk.clerkLock, i); 
+            Release(applicationClerk.senatorLineLock, i);
+            Wait(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);      /* Wait for customer to give application */
+            /*myCustomer = applicationClerk.customerID;*/
+            myCustomer = GetMV(applicationClerk.customerID, i);
             Print("ApplicationClerk %i ", i);
             Print("has recieved SSN %i ", myCustomer);
             Print("from Senator %i\n", myCustomer);
-            Signal(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);    /* Process application */
+            Signal(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);    /* Process application */
             Print("ApplicationClerk %i ", i);
             Print("has recorded a completed application from Senator %i\n", myCustomer);
-            Wait(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);      /* Wait for Customer to accept completed application */
-            /*applicationClerks[i].customerID = -1;*/ 
-            SetMV(applicationClerks[i].customerID, -1);
-            Release(applicationClerks[i].clerkLock);
-            /*applicationClerks[i].state = FREE;*/
-            SetMV(applicationClerks[i].state, FREE);
-    	/*} else if(applicationClerks[i].bribeLineLength > 0) {*/
-    	} else if(GetMV(applicationClerks[i].bribeLineLength) > 0 && !GetMV(senatorInside) && !GetMV(senatorsOutside)) { /*never signal customers if a senator is inside*/
-            Acquire(applicationClerks[i].bribeLineLock);
-            Signal(applicationClerks[i].bribeLineCV, applicationClerks[i].bribeLineLock);     /* Signal Customer to exit line */
+            Wait(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);      /* Wait for Customer to accept completed application */
+            /*applicationClerk.customerID = -1;*/ 
+            SetMV(applicationClerk.customerID, i, -1);
+            Release(applicationClerk.clerkLock, i);
+            /*applicationClerk.state = FREE;*/
+            SetMV(applicationClerk.state, i, FREE);
+    	/*} else if(applicationClerk.bribeLineLength > 0) {*/
+    	} else if(GetMV(applicationClerk.bribeLineLength, i) > 0 && !GetMV(senatorInside, -1) && !GetMV(senatorsOutside, -1)) { /*never signal customers if a senator is inside*/
+            Acquire(applicationClerk.bribeLineLock, i);
+            Signal(applicationClerk.bribeLineCV, i, applicationClerk.bribeLineLock, i);     /* Signal Customer to exit line */
             Print("ApplicationClerk %i has signalled a Customer to come to their counter\n", i);
-            Acquire(applicationClerks[i].clerkLock); 
-            Release(applicationClerks[i].bribeLineLock);
-            Wait(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);      /* Wait for customer to give application */
-            /*myCustomer = applicationClerks[i].customerID;*/
-            myCustomer = GetMV(applicationClerks[i].customerID);
+            Acquire(applicationClerk.clerkLock, i); 
+            Release(applicationClerk.bribeLineLock, i);
+            Wait(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);      /* Wait for customer to give application */
+            /*myCustomer = applicationClerk.customerID;*/
+            myCustomer = GetMV(applicationClerk.customerID, i);
             Print("ApplicationClerk %i ", i);
             Print("has recieved SSN %i ", myCustomer);
             Print("from Customer %i\n", myCustomer);
-            Signal(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);    /* Process application */
+            Signal(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);    /* Process application */
             Print("ApplicationClerk %i ", i);
             Print("has accepted application from Customer %i\n", myCustomer);
-            Wait(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);      /* Wait for Customer to accept completed application */
-            /*applicationClerks[i].customerID = -1;  */
-            SetMV(applicationClerks[i].customerID, -1);  
-            Release(applicationClerks[i].clerkLock);
-            /*applicationClerks[i].state = FREE;*/
-            SetMV(applicationClerks[i].state, FREE);
-        /*} else if(applicationClerks[i].lineLength > 0) {*/
-        } else if(GetMV(applicationClerks[i].lineLength) > 0 && !GetMV(senatorInside) && !GetMV(senatorsOutside)) { /*never signal customers if a senator is inside*/
-            Acquire(applicationClerks[i].lineLock);
-            Signal(applicationClerks[i].lineCV, applicationClerks[i].lineLock);     /* Signal Customer to exit line */
+            Wait(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);      /* Wait for Customer to accept completed application */
+            /*applicationClerk.customerID = -1;  */
+            SetMV(applicationClerk.customerID, i, -1);  
+            Release(applicationClerk.clerkLock, i);
+            /*applicationClerk.state = FREE;*/
+            SetMV(applicationClerk.state, i, FREE);
+        /*} else if(applicationClerk.lineLength > 0) {*/
+        } else if(GetMV(applicationClerk.lineLength, i) > 0 && !GetMV(senatorInside, -1) && !GetMV(senatorsOutside, -1)) { /*never signal customers if a senator is inside*/
+            Acquire(applicationClerk.lineLock, i);
+            Signal(applicationClerk.lineCV, i, applicationClerk.lineLock, i);     /* Signal Customer to exit line */
             Print("ApplicationClerk %i has signalled a Customer to come to their counter\n", i);
-            Acquire(applicationClerks[i].clerkLock); 
-            Release(applicationClerks[i].lineLock);
-            Wait(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);      /* Wait for customer to gve application */
-            /*myCustomer = applicationClerks[i].customerID;*/
-            myCustomer = GetMV(applicationClerks[i].customerID);
+            Acquire(applicationClerk.clerkLock, i); 
+            Release(applicationClerk.lineLock, i);
+            Wait(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);      /* Wait for customer to gve application */
+            /*myCustomer = applicationClerk.customerID;*/
+            myCustomer = GetMV(applicationClerk.customerID, i);
             Print("ApplicationClerk %i ", i);
             Print("has recieved SSN %i ", myCustomer);
             Print("from Customer %i\n", myCustomer);
-            Signal(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);    /* Process application */
+            Signal(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);    /* Process application */
             Print("ApplicationClerk %i ", i);
             Print("has accepted application from Customer %i\n", myCustomer);
-            Wait(applicationClerks[i].clerkCV, applicationClerks[i].clerkLock);		/* Wait for Customer to accept completed application */
-            /*applicationClerks[i].customerID = -1;*/ 
-            SetMV(applicationClerks[i].customerID, -1); 
-            Release(applicationClerks[i].clerkLock);
-            /*applicationClerks[i].state = FREE;*/
-            SetMV(applicationClerks[i].state, FREE);
+            Wait(applicationClerk.clerkCV, i, applicationClerk.clerkLock, i);		/* Wait for Customer to accept completed application */
+            /*applicationClerk.customerID = -1;*/ 
+            SetMV(applicationClerk.customerID, i, -1); 
+            Release(applicationClerk.clerkLock, i);
+            /*applicationClerk.state = FREE;*/
+            SetMV(applicationClerk.state, i, FREE);
         } else {
-            Acquire(applicationClerks[i].clerkLock);
-            /*applicationClerks[i].state = BREAK;*/
-            SetMV(applicationClerks[i].state, BREAK);
+            Acquire(applicationClerk.clerkLock, i);
+            /*applicationClerk.state = BREAK;*/
+            SetMV(applicationClerk.state, i, BREAK);
             Print("ApplicationClerk %i is going on break\n", i);
-            Wait(applicationClerks[i].breakCV, applicationClerks[i].clerkLock);
+            Wait(applicationClerk.breakCV, i, applicationClerk.clerkLock, i);
             Print("ApplicationClerk %i is coming off break\n", i);
-            Release(applicationClerks[i].clerkLock);
-            /*applicationClerks[i].state = FREE;*/
-            SetMV(applicationClerks[i].state, FREE);
-            if(GetMV(timeToLeave)) {
+            Release(applicationClerk.clerkLock, i);
+            /*applicationClerk.state = FREE;*/
+            SetMV(applicationClerk.state, i, FREE);
+            if(GetMV(timeToLeave, -1)) {
                 Print("ApplicationClerk %i is leaving the office.\n",i);
                 Exit(0);
             }
@@ -94,10 +94,10 @@ void runApplicationClerk() {
 int main() {
     Setup();
 
-    Acquire(applicationClerkIndexLock);
-	i = GetMV(nextAvailableApplicationClerkIndex);
-    SetMV(nextAvailableApplicationClerkIndex, i + 1);
-	Release(applicationClerkIndexLock);
+    Acquire(applicationClerkIndexLock, -1);
+	i = GetMV(nextAvailableApplicationClerkIndex, -1);
+    SetMV(nextAvailableApplicationClerkIndex, -1, i + 1);
+	Release(applicationClerkIndexLock, -1);
 	runApplicationClerk();
     
 	Exit(0);
--- test/networkTests.c
+++ test/networkTests.c
@@ -1,33 +1,35 @@
 #include "syscall.h"
 
 void TestWait() {
-	Acquire("lockOne");
-	Wait("conditionOne", "lockOne"); /* lockOne will be released */
+	Acquire("lockOne", -1);
+	Wait("conditionOne", -1, "lockOne", -1); /* lockOne will be released */
 	Print("\tWait test (2/2): Finished waiting\n", 100);
-	Release("lockOne"); /* release lockOne again, Wait re-acquired after Signal. */
+	Release("lockOne", -1); /* release lockOne again, Wait re-acquired after Signal. */
 	Exit(0);
 }
 
 void TestSignal() {
-	Acquire("lockOne");
+	Acquire("lockOne", -1);
 	Print("\tWait test (1/2): Wait DID release the lock\n", 100);
-	Signal("conditionOne", "lockOne"); /* lockOne will be released */
+	Signal("conditionOne", -1, "lockOne", -1); /* lockOne will be released */
+    Release("lockOne", -1);
 	Print("\tSignal test (1/2): Finished signaling\n", 100);
 	Exit(0);
 }
 
 void TestBroadcast() {
-	Acquire("lockOne");
-	Broadcast("conditionOne", "lockOne");
+	Acquire("lockOne", -1);
+	Broadcast("conditionOne", -1, "lockOne", -1);
+    Release("lockOne", -1);
 	Print("\tBroadcast test (?/5): Finished broadcasting\n", 100);
 	Exit(0);
 }
 
 void TestBroadcastHelper() {
-	Acquire("lockOne");
-	Wait("conditionOne", "lockOne"); /* lockOne will be released half-way through Wait, then gotten again */
+	Acquire("lockOne", -1);
+	Wait("conditionOne", -1, "lockOne", -1); /* lockOne will be released half-way through Wait, then gotten again */
 	Print("\tBroadcast test (?/5): Finished waiting\n", 100);
-	Release("lockOne"); /* release lockOne again, Wait re-acquired after Signal. */	
+	Release("lockOne", -1); /* release lockOne again, Wait re-acquired after Signal. */
 	Exit(0);
 }
 
@@ -39,55 +41,55 @@ int main() {
 	NetPrint("Hello World!\n", 13);
 
 	/* Test CreateLock */
-	CreateLock("lockOne");
-	CreateLock("lockTwo");
+	CreateLock("lockOne", -1);
+	CreateLock("lockTwo", -1);
 	Print("\tCreateLock test (1/1): Two locks created\n", 100);
 
 	/* Test (premature) Release */
-	Release("lockOne");
+	Release("lockOne", -1);
 	Print("\tRelease test (1/2): Check server console for \"WARN: Release ...\"\n", 100);
 
 	/* Test Acquire */
-	Acquire("lockOne");
+	Acquire("lockOne", -1);
 	Print("\tAcquire test (1/1): Lock acquired\n", 100);
 
 	/* Test Release */
-	Release("lockOne");
+	Release("lockOne", -1);
 	Print("\tRelease test (2/2): Lock released\n", 100);
 
 	/* Test DestroyLock */
-	DestroyLock("lockOne");
-	DestroyLock("lockTwo");
+	DestroyLock("lockOne", -1);
+	DestroyLock("lockTwo", -1);
 	Print("\tDestroyLock test (1/1): Destroyed two locks\n", 100);
 
 	/* Test CreateCondition */
-	CreateCondition("conditionOne");
+	CreateCondition("conditionOne", -1);
 	Print("\tCreateCondition test (1/1): One condition created\n", 100);
 
 	/* Test Wait / Signal */
-	CreateLock("lockOne");
+	CreateLock("lockOne", -1);
 	Fork(&TestWait);
 	Fork(&TestSignal);
-	Acquire("lockOne");	/* Somehow lockOne is being acquired before it's apropriate */
+	Acquire("lockOne", -1);	/* Somehow lockOne is being acquired before it's apropriate */
 	Print("\tSignal test (2/2): Signal DID release the lock\n", 100);
-	Release("lockOne");
+	Release("lockOne", -1);
 
 	/* Test Broadcast */
 	Fork(&TestBroadcastHelper);
 	Fork(&TestBroadcastHelper);
-	Acquire("lockOne");
+	Acquire("lockOne", -1);
 	Print("\tBroadcast test (1/5): Acquired lock to prove waits DID release\n", 100);
-	Release("lockOne");
+	Release("lockOne", -1);
 	Fork(&TestBroadcast);
-	Acquire("lockOne");
+	Acquire("lockOne", -1);
 	Print("\tBroadcast test (5/5): Broadcast DID release the lock\n", 100);
-	Release("lockOne");
+	Release("lockOne", -1);
 
 	/* Test DestroyCondition */
-	DestroyCondition("conditionOne");
+	DestroyCondition("conditionOne", -1);
 	Print("\tDestroyCondition test (1/1): Destroyed one condition\n", 100);
-	Acquire("lockOne"); /* Don't destroy a lock held by someone else */
-	DestroyLock("lockOne");
+	Acquire("lockOne", -1); /* Don't destroy a lock held by someone else */
+	DestroyLock("lockOne", -1);
 
 	/* Test NetHalt */
 	Print("\tNetHalt test (1/1): Check that server shuts down\n", 100);
