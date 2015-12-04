775496d6f795ad4c708f27770e227b5ea77976a0
Yetsebaot Sisay <sisay@usc.edu>
2015-12-03 23:30:01 -0800

    added testMV.c to test monitor variables

--- test/testMV.c
+++ test/testMV.c
@@ -4,41 +4,41 @@ int mv1, mv2, val;
 
 int main() {
 	Print("Test regular creation & use\n", 0);
-	mv1 = CreateMV();
-	mv2 = CreateMV();
+	CreateMV("mv1",-1);
+	CreateMV("mv1", -1);
 	Print("Created two MVs: mv1 & mv2\n", 0);
 
 
-	val = GetMV(mv1);
+	val = GetMV(mv1, -1);
 	Print("mv1->val: %d\n", val);
 
-	val = GetMV(mv2);
+	val = GetMV(mv2, -1);
 	Print("mv2->val: %d\n", val);
 
 
 	Print("Testing SetMV\n", 0);
 	Print("Setting mv1->val to 3\n", 0);
-	SetMV(mv1, 3);
+	SetMV(mv1, -1, 3);
 	Print("Setting mv2->val to 9\n", 0);
-	SetMV(mv2, 9);
+	SetMV(mv2, -1, 9);
 
-	val = GetMV(mv1);
+	val = GetMV(mv1, -1);
 	Print("mv1->val: %d\n", val);
 
-	val = GetMV(mv2);
+	val = GetMV(mv2, -1);
 	Print("mv2->val: %d\n", val);
 	Print("SetMV successful\n", 0);
 
 
 	Print("Destroying mv1\n", 0);
-	DestroyMV(mv1);
+	DestroyMV("mv1", -1);
 
 	Print("Destroying mv2\n", 0);
-	DestroyMV(mv2);
+	DestroyMV("mv2", -1);
 
 	Print("Test misuse\n", 0);
 	Print("Try setting mv2 - Look for WARN: 'GetMV failed. No such MV.'\n", 0);
-	SetMV(mv2, 100);
+	SetMV(mv2, -1, 100);
 
 	Exit(0);
 }
